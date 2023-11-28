94b489ebbe50d4e3c9da46e0288ea13f87db363a
pablo <pablo.tsanchez@gmail.com>
2023-11-29 00:39:37 +0100

    feat: move fetchPage out of BookView into BookPage, small UI polishing

--- src/components/BookPage.vue
+++ src/components/BookPage.vue
@@ -1,45 +1,93 @@
 <template>
   <div>
-    <div class="flex flex-row gap-4 mt-1 mb-6">
-      <div class="basis-1/2 text-neutral-400 text-justify">&emsp;{{ text }}</div>
-      <div clasS="basis-1/2"><img v-if="imgSrc" :src="imgSrc"/></div>
+    <div v-if="text">
+      <div class="flex flex-row gap-4 mt-1 mb-6">
+        <div class="basis-1/2 text-neutral-400 text-justify">&emsp;{{ text }}</div>
+        <div clasS="basis-1/2"><img v-if="imgSrc" :src="imgSrc"/></div>
+      </div>
+      <div v-if="receivedInput">
+        <input type="text" :value="receivedInput" disabled class="text-neutral-800 rounded-md w-full bg-neutral-500"/>
+        <button v-if="!nextClicked" type="button" class="mt-3 rounded-md p-2.5 bg-sky-700 text-neutral-100" @click="clickNext">
+          Read next page
+        </button>
+      </div>
+      <div v-else>
+        <label class="block">
+          <span class="text-neutral-500">Your input</span>
+          <input type="text" v-model="createdInput" :disabled="nextClicked" class="text-neutral-800 rounded-md w-full bg-neutral-500 focus:bg-neutral-300 focus:border-neutral-400 focus:ring-0"/>
+        </label>
+        <button v-if="!nextClicked" type="button" class="mt-3 rounded-md p-2.5 bg-sky-700 text-neutral-100" @click="sendInput">
+          Generate next page
+        </button>
+      </div>
     </div>
-    <div v-if="receivedInput">
-      <input type="text" class="text-neutral-800 rounded-md w-full bg-neutral-500" :value="receivedInput" disabled/>
-      <button v-if="!nextClicked" type="button" class="mt-3 rounded-md p-2.5 bg-sky-700 text-neutral-100" @click="clickNext">
-        Read next page
-      </button>
+    <div v-else-if="isPolling">
+      <!-- Loading -->
+      Loading
     </div>
     <div v-else>
-      <label class="block">
-        <span class="text-neutral-500">Your input</span>
-        <input type="text" v-model="createdInput" class="text-neutral-800 rounded-md w-full bg-neutral-500 focus:bg-neutral-300 focus:border-neutral-400 focus:ring-0"/>
-      </label>
-      <button type="button" class="mt-3 rounded-md p-2.5 bg-sky-700 text-neutral-100" @click="sendInput">
-        Generate next page
-      </button>
+      Error
     </div>
   </div>
 </template>
 
 <script>
-import { post } from '../actions'
+import { get, post } from '../actions'
+
+const MAX_POLLING_ATTEMPTS = 10
 
 export default {
   name: 'BookPage',
   props: {
     bookId: String,
-    text: String,
-    receivedInput: String,
-    imgSrc: String,
+    pageNumber: Number,
   },
   data() {
     return {
+      text: '',
+      imgSrc: '',
+      footer: '',
+      receivedInput: '',
       createdInput: '',
       nextClicked: false,
+      pollingCount: 0,
+    }
+  },
+  mounted() {
+    this.fetchPage()
+  },
+  computed: {
+    isPolling() {
+      return this.pollingCount < MAX_POLLING_ATTEMPTS
     }
   },
   methods: {
+    fetchPage() {
+      get("page", {book_id: this.bookId, page_number: this.pageNumber})
+        .then((response) => {
+          if (response.text) {
+            this.text = response.text
+            this.receivedInput = response.input
+            this.imgSrc = response.img
+            this.footer = response.footer
+          } else {
+            this.startPolling()
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+        .finally(() => {
+        })
+    },
+    startPolling() {
+      this.pollingCount++
+      if (this.pollingCount < MAX_POLLING_ATTEMPTS) {
+        setTimeout(() => {
+          this.fetchPage()
+        }, 1000);
+      }
+    },
     sendInput() {
       const params = {
         book_id: this.bookId,
@@ -48,6 +96,8 @@ export default {
       post("create-page", params)
         .then((response) => {
           console.log(response)
+          this.nextClicked = true
+          this.$emit("next")
         })
         .catch((err) => {
           console.log(err)
--- src/components/BookView.vue
+++ src/components/BookView.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
-    <BookPage v-for="(page, pageId) in loadedPages"
-      :key="pageId"
+    <BookPage v-for="pageNumber in loadedPages"
+      :key="pageNumber"
       :bookId="bookId"
-      :text="page.text"
-      :receivedInput="page.input"
-      @next="this.getPage"
+      :pageNumber="pageNumber"
+      @next="this.addPage"
     />
   </div>
 </template>
 
 <script>
-import { get } from '../actions'
 import BookPage from './BookPage.vue'
 
 export default {
@@ -25,30 +23,16 @@ export default {
   data() {
     return {
       lastPage: 1,
-      loadedPages: {},
+      loadedPages: [],
     }
   },
   mounted() {
-    this.getPage()
+    this.addPage()
   },
   methods: {
-    getPage() {
-      get("page", {book_id: this.bookId, page_number: this.lastPage})
-      .then((response) => {
-        this.loadedPages[response.id] = {
-          text: response.text,
-          input: response.input,
-          number: response.number,
-          img: response.img,
-          footer: response.footer,
-        }
-        this.lastPage++
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-      .finally(() => {
-      })
+    addPage() {
+      this.loadedPages.push(this.lastPage)
+      this.lastPage++
     }
   },
 }
